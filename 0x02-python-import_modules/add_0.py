bb2c4ccf789f6c254bcd4fbd06d99cfaf5c64145
S1M0BH <mohamedbchiri90@gmail.com.com>
2023-06-08 19:03:47 +0000

    0x02

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+        The return value. a + b
+    """
+    return (a + b)
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """
+        The return value. a / b
+    """
+    return int(a / b)
