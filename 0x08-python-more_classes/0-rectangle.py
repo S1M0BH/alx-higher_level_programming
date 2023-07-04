fa07098b741208c12877e14d593eeac193780054
S1M0BH <mohamedbchiri90@gmail.com>
2023-07-04 00:00:24 +0000

    0x08

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" defines rectangle (class)"""
+"""A class that defines a rectangle"""
+
 
 class Rectangle:
-    """ Rectangle (class)"""
-
+    """An empty class that represents a rectangle"""
     pass
