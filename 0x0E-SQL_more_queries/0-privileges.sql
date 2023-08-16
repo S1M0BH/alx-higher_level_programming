6b6b3b6dfd60362f9f692c213304eaf5453126e0
S1M0BH <mohamedbchiri90@gmail.com.com>
2023-08-16 16:18:24 +0000

    0x0E

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all MySQL user privileges user_0d_1 and user_0d_2
+-- Lists all MySQL user privileges user 0d 1 and user 0d 2
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
